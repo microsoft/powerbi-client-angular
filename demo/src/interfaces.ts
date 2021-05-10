9131aebbd0c3cd7de30130357ccd67b456f2cf4b
Parth Mangukiya <v-parman@microsoft.com>
2021-05-10 12:45:53 +0530

    Minor code improvements

--- demo/src/interfaces.ts
+++ demo/src/interfaces.ts
@@ -1,12 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-// Handles the embed config response for Tile embedding
+// Handles the embed config response for embedding
 export interface ConfigResponse {
   Id: string;
   EmbedUrl: string;
   EmbedToken: {
     Token: string;
   };
-  DashboardId?: string;
 }
