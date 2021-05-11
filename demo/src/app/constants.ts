81f20e914107d25671076ff23ece882302de711d
Parth Mangukiya <v-parman@microsoft.com>
2021-05-11 10:17:53 +0530

    Removed unused endpoints

--- demo/src/app/constants.ts
+++ demo/src/app/constants.ts
@@ -1,8 +1,4 @@
-const dashboardUrl = 'https://playgroundbe-bck-1.azurewebsites.net/Dashboards/SampleDashboard';
-const paginatedReportUrl = 'https://playgroundbe-bck-1.azurewebsites.net/Reports/SampleRdlReport';
-const qnaUrl = 'https://playgroundbe-bck-1.azurewebsites.net/Datasets/SampleQna';
-const datasetUrl = 'https://playgroundbe-bck-1.azurewebsites.net/Reports/SampleCreate';
-const reportUrl = 'https://aka.ms/CaptureViewsReportEmbedConfig';
-const tileUrl = 'https://playgroundbe-bck-1.azurewebsites.net/Tiles/SampleTile';
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
-export { dashboardUrl, paginatedReportUrl, qnaUrl, datasetUrl, reportUrl, tileUrl };
+export const reportUrl = 'https://aka.ms/CaptureViewsReportEmbedConfig';
