8270697dbbe44773c0e1137799ed05dc999c560a
Parth Mangukiya <v-parman@microsoft.com>
2021-05-12 11:40:21 +0000

    Merged PR 169334: [Angular wrapper]: Show only Report component in demo
    This PR updates the demo-app to just have the Report component in the demo-app.
    
    ![Demo-app.gif](https://powerbi.visualstudio.com/4c7b5adb-c2d0-4f18-b23c-edc4ac30f4e1/_apis/git/repositories/3675b5a7-9df2-4f4f-a21b-14cb44be8ee9/pullRequests/169334/attachments/Demo-app.gif)
    
    Related work items: #575786, #579912

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -2,10 +2,102 @@
 // Licensed under the MIT License.
 
 import { Component } from '@angular/core';
+import { IReportEmbedConfiguration, models, service } from 'powerbi-client';
+import { HttpService } from 'src/app/services/http.service';
+import { reportUrl } from './constants';
+
+// Handles the embed config response for embedding
+export interface ConfigResponse {
+  Id: string;
+  EmbedUrl: string;
+  EmbedToken: {
+    Token: string;
+  };
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  // Overall status message of embedding
+  displayMessage = 'The report is bootstrapped. Click Embed Report button to set the access token.';
+
+  // CSS Class to be passed to the wrapper
+  reportClass = 'report-container';
+
+  // Flag which specify the type of embedding
+  phasedEmbeddingFlag = false;
+
+  // Pass the basic embed configurations to the wrapper to bootstrap the report on first load
+  // Values for properties like embedUrl, accessToken and settings will be set on click of button
+  reportConfig: IReportEmbedConfiguration = {
+    type: 'report',
+    embedUrl: undefined,
+    tokenType: models.TokenType.Embed,
+    accessToken: undefined,
+    settings: undefined,
+  };
+
+  /**
+   * Map of event handlers to be applied to the embedded report
+   */
+  // Update event handlers for the report by redefining the map using this.eventHandlersMap
+  // Set event handler to null if event needs to be removed
+  // More events can be provided from here
+  // https://docs.microsoft.com/en-us/javascript/api/overview/powerbi/handle-events#report-events
+  eventHandlersMap = new Map<string, (event?: service.ICustomEvent<any>) => void>([
+    ['loaded', () => console.log('Report has loaded')],
+    [
+      'rendered',
+      () => {
+        console.log('Report has rendered');
+
+        // Update display message
+        this.displayMessage = 'The report is rendered.';
+      },
+    ],
+    [
+      'error',
+      (event?: service.ICustomEvent<any>) => {
+        if (event) {
+          console.error(event.detail);
+        }
+      },
+    ],
+    ['visualClicked', () => console.log('visual clicked')],
+    ['pageChanged', (event) => console.log(event)],
+  ]);
+
+  constructor(public httpService: HttpService) {}
+
+  /**
+   * Embeds report
+   *
+   * @returns Promise<void>
+   */
+  async embedReport(): Promise<void> {
+    let reportConfigResponse: ConfigResponse;
+
+    // Get the embed config from the service and set the reportConfigResponse
+    try {
+      reportConfigResponse = await this.httpService.getEmbedConfig(reportUrl).toPromise();
+    } catch (error) {
+      this.displayMessage = `Failed to fetch config for report. Status: ${error.statusText} Status Code: ${error.status}`;
+      console.error(this.displayMessage);
+      return;
+    }
+
+    // Update the reportConfig to embed the PowerBI report
+    this.reportConfig = {
+      ...this.reportConfig,
+      id: reportConfigResponse.Id,
+      embedUrl: reportConfigResponse.EmbedUrl,
+      accessToken: reportConfigResponse.EmbedToken.Token,
+    };
+
+    // Update the display message
+    this.displayMessage = 'Access token is successfully set. Loading Power BI report.';
+  }
+}
